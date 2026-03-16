b6a41927e5d7be8e5105bb20505b9439a815ceb9
JocelyneGonzalezHernandez <128755176+JocelyneGonzalezHernandez@users.noreply.github.com>
2026-03-15 18:01:58 -0600

    Add files via upload

---
+++ Bases de datos/AJAX/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * gestion_int Configuration File
+ */
+
+/**
+ * Server configuration
+ */
+$cfgServer['host']	= 'localhost';	// MySQL hostname
+$cfgServer['user']	= '';	// MySQL user
+$cfgServer['password']	= '';		// MySQL password 
+$cfgServer['dbname']	= 'presidentes';	// MySQL database name
+?>
